6627783f3fcaaf4bca0b0e05a5dd354fac5ef513
hojuly <hojune.choi.me@gmail.com>
2021-06-23 17:30:24 +0900

    최솟값 구하기 SQL

---
+++ sql/lv2_210623.sql
@@ -0,0 +1,14 @@
+-- 최솟값 구하기
+
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+
+SELECT MIN(DATETIME) AS "시간"
+FROM ANIMAL_INS;
+
+-- OR
+
+SELECT DATETIME AS "시간"
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
+
